2bfe08113920b06e48f8c939f0fef5bd0bb49972
amadan171 <150274224+amadan171@users.noreply.github.com>
2024-02-20 16:59:05 -0500

    Add files via upload
    Program to calculate minimum and maximum of a set of numbers

---
+++ src/MinMax.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Calc {
+
+    public static void main(String args[]) {
+        int a[] = {11, 18, 3, 5, 1};
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int x : a) {
+            if(x < min)
+                min = x;
+            if(x > max)
+                max = x;
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}
